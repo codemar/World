8e3aad1c50c95021b383386e818fc13f4eff1fb1
Omar Zeidan <omar@zeidan.io>
2017-05-02 05:53:01 +0200

    Minor stuff.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
 document.addEventListener("DOMContentLoaded", initialize);
 
 let myHero;
-let world;
+// let world;
 let sidebar;
 let heroBuilder;
+let client;
 
 function initialize() {
     document.body.style.margin = '0px'; // remove margin
     
     sidebar = new Sidebar();
-    world = new World();
+    // world = new World();
     heroBuilder = new HeroBuilder();
+    client = new Client("ws://localhost:2794/");
     
     const createfun = function() {
         myHero = new Hero({x: 5, y: 5}, heroBuilder.character);
-        world.addHero(myHero);
+        // world.addHero(myHero);
+
+        client.connect();
+        client.setHeroPixels(myHero.blocks, myHero.width, myHero.height);
 
         enableMovement();
         
