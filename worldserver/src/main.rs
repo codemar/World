504d4f29bb79d35d7e540363982cc03ceb27c10f
Omar Zeidan <omar@zeidan.io>
2017-05-10 01:39:50 +0200

    Moved handling the connections to own fileqq

--- worldserver/src/main.rs
+++ worldserver/src/main.rs
@@ -1,53 +1,9 @@
 extern crate websocket;
 
-use std::thread;
-use websocket::{Server, Message};
-use websocket::message::Type;
+mod canvas;
+mod color;
+mod server;
 
 fn main() {
-    let server = Server::bind("127.0.0.1:2794").unwrap();
-
-    for request in server.filter_map(Result::ok) {
-	// Spawn a new thread for each connection.
-	thread::spawn(move || {
-	    if !request.protocols().contains(&"rust-websocket".to_string()) {
-		request.reject().unwrap();
-		return;
-	    }
-
-	    let mut client = request.use_protocol("rust-websocket").accept().unwrap();
-
-	    let ip = client.peer_addr().unwrap();
-
-	    println!("Connection from {}", ip);
-
-	    let message: Message = Message::text("Hello".to_string());
-	    client.send_message(&message).unwrap();
-
-	    let (mut receiver, mut sender) = client.split().unwrap();
-
-	    for message in receiver.incoming_messages() {
-		let message: Message = message.unwrap();
-
-		match message.opcode {
-		    Type::Close => {
-			let message = Message::close();
-			sender.send_message(&message).unwrap();
-			println!("Client {} disconnected", ip);
-			return;
-		    }
-		    Type::Ping => {
-			let message = Message::pong(message.payload);
-			sender.send_message(&message).unwrap();
-		    }
-
-                    Type::Binary => {
-                        println!("Received binary data!");
-                        // Do something with it
-                    }
-		    _ => sender.send_message(&message).unwrap(),
-		}
-	    }
-	});
-    }
+    server::server_loop();
 }
