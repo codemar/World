45c105d42193aed2ca48a28cb42f9bf70a46d88f
Omar Zeidan <omar@zeidan.io>
2017-05-12 19:06:22 +0200

    Further worked on world.rs

--- worldserver/src/world.rs
+++ worldserver/src/world.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use color;
 use canvas;
 use position;
@@ -7,20 +7,30 @@ type Color = color::Color;
 type Canvas = canvas::Canvas;
 type Pos = position::Pos;
 
+#[derive(Copy, Clone)]
 pub struct Interval {
     l: u32,
     u: u32
 }
 
 struct World {
-    map : HashMap<Pos, Color>,
+    colorMap : HashMap<Pos, Color>,
+    colissionMap : HashSet<Pos>,
     loadedintervals : Vec<Interval>
 }
 
 impl World {
     pub fn insertCanvas(&mut self, canvas: &Canvas) {
         for (position, color) in canvas {
-            self.map.insert(position, color);
+            self.colorMap.insert(position, color);
+        }
+    }
+
+    pub fn insertColor(&mut self, position: Pos, color: Color, collides: bool) {
+        self.colorMap.insert(position, color);
+
+        if collides {
+            self.colissionMap.insert(position);
         }
     }
 }
