a1f0d421f8a5e8456c370e90694cfe80cc48d60a
Omar Zeidan <omar@zeidan.io>
2017-05-12 18:22:20 +0200

    Derived copy instead of implementing.

--- worldserver/src/color.rs
+++ worldserver/src/color.rs
@@ -1,21 +1,12 @@
 use std::fmt;
 
+#[derive(Copy,Clone)]
 pub struct Color {
     pub red: u8,
     pub green: u8,
     pub blue: u8,
 }
 
-
-impl Copy for Color {
-    
-}
-
-impl Clone for Color {
-    fn clone(&self) -> Color {
-        *self
-    }
-}
 impl fmt::Display for Color {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "({}, {}, {})", self.red, self.green, self.blue)
