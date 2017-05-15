6f001267cf346494ba21e8ab0ff6fe099e7931e2
Omar Zeidan <omar@zeidan.io>
2017-05-15 15:44:40 +0200

    Added functionality for color struct.

--- worldserver/src/color.rs
+++ worldserver/src/color.rs
@@ -1,6 +1,7 @@
 use std::fmt;
+use std::default::Default;
 
-#[derive(Copy,Clone)]
+#[derive(Copy,Clone, Debug)]
 pub struct Color {
     pub red: u8,
     pub green: u8,
@@ -9,6 +10,17 @@ pub struct Color {
 
 impl fmt::Display for Color {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {}, {})", self.red, self.green, self.blue)
+        write!(f, "(r: {}, g: {}, b: {})", self.red, self.green, self.blue)
+    }
+}
+
+
+impl Default for Color {
+    fn default() -> Color { Color{red: 255, green: 255, blue: 255}}
+}
+
+impl Color {
+    pub fn from_bytes(bytes: &[u8]) -> Color {
+        Color{red: bytes[0], green: bytes[1], blue: bytes[2]}
     }
 }
